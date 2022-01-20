b300a8af673b9ceb15696fa4df8dfa200d3563e3
d2gex <daniel.garcia@d2garcia.com>
2022-01-20 07:20:27 +0100

    Add half-way chlorophyll business logic

--- oceanografia_biologica/light_exercise.R
+++ oceanografia_biologica/light_exercise.R
@@ -17,6 +17,20 @@ read_data <- function(data_path) {
 }
 
 
+get_closest_pair_to_value <- function(vector, value) {
+  #'@description get the pair of closest values to a vector
+  return(vector[abs(vector-value) %in% sort(abs(vector-value))[1:2]])
+}
+
+calculate_vector_pair_substraction <- function(vector, up_to) {
+  #'@description subtract every two pairs in a vector
+  
+  vector_dup <-c(0, vector[1:up_to], 0)
+  return (c(0, abs(vector - vector_dup)[-c(1, length(vector))], 0))
+  
+  
+}
+
 prepare_df <- function(ch_data) {
   # Prepare data frame for post-processing purposes
 
@@ -52,20 +66,18 @@ add_i0_to_df <- function(ch_data) {
   col_names <- c(col_names[1:i_z_offset],
                  col_names[i_0_offset],
                  col_names[(i_z_offset + 1):(i_0_offset - 1)])
-  print(col_names)
   ch_data = ch_data[, col_names]
   return(ch_data)
 
 }
 
 add_a_z_to_df <- function(ch_data) {
-  #'@description find the increments between discreed depths
+  #'@description find the increments between depths
   
   # Find increments of z, making the first and last as NA as they aren't needed
-  z_1 <- ch_data$z
-  up_to = length(z_1) - 2
-  z_2 <-c(0, z_1[1:up_to], 0)
-  a_z <- c(0, abs(z_1 - z_2)[-c(1, length(z_1))], 0)
+  z = ch_data$z
+  up_to = length(z) - 2
+  a_z = calculate_vector_pair_substraction(z, length(z) - 2)
   return(transform(ch_data, a_z=a_z))
   
 }
@@ -75,21 +87,52 @@ add_k_to_df <- function(ch_data) {
   #'add it to the dataframe. 
   
   # crop i_z, i_0 and z to the [second, penultimate] rows
-  i_z <- ch_data$i_z[-c(1, length(i_z))]
-  i_0 <- ch_data$i_0[-c(1, length(i_0))]
-  a_z <- ch_data$a_z[-c(1, length(a_z))]
+  i_z <- ch_data$i_z[-c(1, length(ch_data$i_z))]
+  i_0 <- ch_data$i_0[-c(1, length(ch_data$i_0))]
+  a_z <- ch_data$a_z[-c(1, length(ch_data$a_z))]
   
   # calculate k as the log difference over z
-  k<- (log(i_z) - log(i_0)) / log(a_z)
-  print(k)
+  k<- (log(i_z) - log(i_0)) / a_z
   # repopulate k to havfe te same dimension as the length of i_z/i_0 again
   return(transform(ch_data, k = c(NA, k, NA)))
   
 }
 
 calculate_k <- function(ch_data){
+  print(ch_data$k)
   return(mean(ch_data$k, na.rm = TRUE))
 }
+
+calculate_photic_depth <- function(ch_data, k) {
+  #'@description calculate the depth of the photic zone based in 1/100 of i_0
+  i_0 = ch_data$i_0[1]
+  photic_i_z = (1/100) * i_0
+  return((log(photic_i_z / i_0) / k))
+}
+
+calculate_chlorophyll_m2 <- function(ch_data, photic_depth) {
+  #'@description Calculate the amount of chlorophyll in mg/m2 using trapezium
+  #'method
+  
+  # Get chlorophyll and z increments from [2, right below photic zone]
+  z = ch_data$z
+  up_to_photic_zone <- (!is.na(z)) & (z < photic_depth) 
+  a_z <- z[up_to_photic_zone][2:length(a_z)]
+  chlo <- ch_data$chlorophyll[up_to_photic_zone][2:length(a_z)]
+  
+  # Get chlorophyll and z increments around photic zone
+  z_pair_around_photic <- get_closest_pair_to_value(z, photic_depth)
+  around_photic_zone <-ch_data$z %in% z_pair_around_photic
+  a_z_additional <- ch_data$a_z [around_photic_zone]
+  chlo_additional <- ch_data$chlorophyll [around_photic_zone]
+  
+  # Calculate trapezium formula for interval [2, right below photic zone]
+  
+  
+  return(list(a_z, a_z_additional, chlo, chlo_additional))
+}
+
+
 
 draw <- function(ch_data) {
   ggplot(data = ch_data, mapping= aes(x = z)) +
@@ -106,7 +149,11 @@ chlorophyll_data = add_i0_to_df(chlorophyll_data)
 chlorophyll_data = add_a_z_to_df(chlorophyll_data)
 chlorophyll_data = add_k_to_df(chlorophyll_data)
 k_coeficient = calculate_k(chlorophyll_data)
+photic_depth = calculate_photic_depth (chlorophyll_data, k_coeficient)
+details = calculate_chlorophyll_m2(chlorophyll_data, photic_depth)
+
 print(k_coeficient)
+print(photic_depth)
 View(chlorophyll_data)
 
 
