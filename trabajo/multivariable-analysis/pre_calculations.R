c3b85b2d5ea81b5e3ab2eabfd135935f00a080d1
Dani from Window <daniel.garcia@d2garcia.com>
2022-04-16 18:33:24 +0100

    generate anonymous transect matrix

--- trabajo/multivariable-analysis/pre_calculations.R
+++ trabajo/multivariable-analysis/pre_calculations.R
@@ -1,9 +1,20 @@
 library("readxl")
 library("dplyr")
 library("plyr")
+
+# Read data
 coral_species <- read.csv2("trabajo/multivariable-analysis/data/corals_spec.csv")
+
+# Calculate the total covers per year rather per year and transect
 data <- coral_species %>% mutate(years = paste("Y", substr(.[[1]], 2,3)))
 data <- data[, c(-1)] %>% select(years, everything())
 data <- aggregate(. ~ years, data = data, FUN = "sum")
-write.csv2(data, "trabajo/multivariable-analysis/data/corals_spec_totals.csv")
+write.csv(data, "trabajo/multivariable-analysis/data/corals_spec_totals.csv")
+
+# Rename matrix so that transect names disappear. We are only interested in
+# having anonymous transects within each year
+
+anonymous_transects <- coral_species %>% mutate(years = substr(.[[1]],3,3))
+anonymous_transects <- anonymous_transects[, c(-1)] %>% select(years, everything())
+write.csv(data, "trabajo/multivariable-analysis/data/corals_anonymos_transects.csv")
 
